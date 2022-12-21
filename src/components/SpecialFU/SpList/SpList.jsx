71acc011b8205c075ddffdd76d166cc6a8d56944
Nikita P <nikita.panasevich22@gmail.com>
2022-12-21 15:28:41 +0200

    Update SpList.jsx

--- src/components/SpecialFU/SpList/SpList.jsx
+++ src/components/SpecialFU/SpList/SpList.jsx
@@ -20,20 +20,6 @@ const SpList = ({data}) => {
           </div>
       </li>
       ))}
-                        {/* <li class="coffee-list__item foryou-list__item">
-                            <img  src="./imgs/sandwich.png" alt="Sandwich"/>
-                            <div >
-                                <h3 >Sandwich</h3>
-                                <p class="coffee-li-item__price">15$</p>
-                            </div>
-                            <div >
-                                <p class="foryou-list-item__desc">bread with meat and vegetables</p>
-                                <div >
-                                    <p>4.8</p>
-                                    <img  src={star} alt="star"/>
-                                </div>
-                            </div>
-      </li> */}
     </ul>
   )
 }
