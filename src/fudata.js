884c2828951b6deb301ec104059bc2d36fd28f78
Nikita P <nikita.panasevich22@gmail.com>
2022-12-21 20:49:53 +0200

    changes to data

--- src/fudata.js
+++ src/fudata.js
@@ -1,48 +1,51 @@
-import vlate from './images/vanillalatte.png'
-import hazlate from './images/hazelnut-latte.png'
-import esp from './images/espresso.png'
+import sandw from './images/sandwich.png'
+import hotmilk from './images/hotmilk.png'
+import coficecreem from './images/coffee-icecream.png'
+import cappuch from './images/sandwich.png'
+import moccach from './images/sandwich.png'
+import waflicecream from './images/sandwich.png'
 
 const data = [{
     name: "Sandwich",
     desc: "bread with meat and vegetables",
     price:15,
     rate:4.8,
-    link:vlate,
+    link:sandw,
 },
 {
     name: "Hot Milk",
     desc: "Hot Milk with less sugar",
     price:12,
     rate:4.8,
-    link:esp,
+    link:hotmilk,
     },
 {
     name: "Coffe Ice Cream",
     desc: "Coffe with ice cream vanilla",
     price:13,
     rate:4.8,
-    link:hazlate,
+    link:coficecreem,
     },
 {
     name: "Cappucino",
     desc: "Hot Cappucino",
     price:15,
     rate:4.8,
-    link:vlate,
+    link:cappuch,
 },
 {
     name: "Moccacinno",
     desc: "Hot Moccacino ",
     price:12,
     rate:4.8,
-    link:esp,
+    link:moccach,
     },
 {
     name: "Waffle Ice Crem",
     desc: "Wafle with Ice cream",
     price:13,
     rate:4.8,
-    link:hazlate,
+    link:waflicecream,
 }]
 
 export default data
