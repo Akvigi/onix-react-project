7a86e339dbb748d5e28319b46fe66ff1cbcfcaae
Nikita P <nikita.panasevich22@gmail.com>
2023-01-05 12:59:01 +0200

    added pagination in reviews section and minor styling

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,46 +1,33 @@
 import React, { useState } from 'react'
 import style from './Reviews.module.sass'
 import data from '../../reviewsdata'
+
 const Reviews = () => {
-    // const [list, setList] = useState([])
+    const [pagePag, setPagePag] = useState(1)
+    console.log(data)
   return (
       <section className={style.Section}>
           <div className={style.Container}>
             <div className={style.TextCont}>
                 <h3 className={style.Heading}>What they say about us</h3>
                 <p className={style.Text}>We always provide the best service and always maintain the quality of coffee</p>
-            </div>
-            <ul className={style.List}>
-              {data.map(({name, desc, link}) => <li className={style.Item}>
-                  <img className={style.Img} src={link} alt={name} />
-                  <div className={style.RevDesc}>
-                      <h4>{name}</h4>
-                      <p>{desc}</p>
-                  </div>
-              </li>)}
-                {/* <li>
-                    <img src={data[0].link} alt="" />
-                    <div>
-                        <h4>{data[0].name}</h4>
-                        <p>{data[0].desc}</p>
-                    </div>
-                </li> 
-                <li>
-                    <img src={data[1].link} alt="" />
-                    <div>
-                        <h4>{data[1].name}</h4>
-                        <p>{data[1].desc}</p>
-                    </div>
-                </li> 
-                <li>
-                    <img src={data[2].link} alt="" />
-                    <div>
-                        <h4>{data[2].name}</h4>
-                        <p>{data[2].desc}</p>
-                    </div>
-                </li>  */}
-                {/* <button type='button' className={style.Btn}></button> */}
-              </ul>
+              </div>
+              <div className={style.ListBtnsCont}>
+                  <ul className={style.List}>
+                    {data.find(({page}) => pagePag === page).data.map(({ name, desc, link }) =>
+                        <li className={style.Item} key={desc}>
+                          <img className={style.Img} src={link} alt={name} />
+                          <div className={style.RevDesc}>
+                              <h4>{name}</h4>
+                              <p>{desc}</p>
+                          </div>
+                      </li>)}
+                </ul>
+                <div className={style.BtnCont}>
+                    {data.map(({ page }) => <button type='button' onClick={() => setPagePag(page)} className={page === pagePag ? `${style.Btn} ${style.BtnActive}` : `${style.Btn}`}></button>)}
+                </div>
+              </div>
+              
             </div>
         </section>
   )
--- src/reviewsdata.js
+++ src/reviewsdata.js
@@ -3,6 +3,8 @@ import t2 from "./images/t2.png"
 import t3 from "./images/t3.png"
 
 const data = [{
+    page: 1,
+    data:[{
     name:"Naura",
     desc:"I really love the cappucino, the coffee was very smooth",
     link:t1,
@@ -16,6 +18,44 @@ const data = [{
     name:"Azura",
     desc:"the coffee menu here is very much",
     link:t3,
-}]
+    }]
+},
+{
+    page: 2,
+    data:[{
+    name:"Dana",
+    desc:"the coffee was very good",
+    link:t1,
+    },
+    {
+    name:"Din",
+    desc:"this coffee is very aromatic",
+    link:t2,
+    },
+    {
+    name:"Dana",
+    desc:"menu is very big",
+    link:t3,
+    }]
+    },
+    {
+    page: 3,
+    data:[{
+    name:"Alana",
+    desc:"I really love the latte, the coffee was very tasty",
+    link:t1,
+    },
+    {
+    name:"Ken",
+    desc:"this coffee shop is the best in town",
+    link:t2,
+    },
+    {
+    name:"Lana",
+    desc:"I am attending this coffee shop every day",
+    link:t3,
+    }]
+},
+]
 
 export default data
