7d675f83eb4503d86ae152ab6c83fc7d82d02dda
Nikita P <nikita.panasevich22@gmail.com>
2023-03-01 15:30:09 +0200

    test

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,27 @@
+import {createSelector} from '@reduxjs/toolkit';
+import {filterStatus} from './constants';
+
 export const getOrder = state => state.order;
+export const getFilter = state => state.filter;
+
+export const getSortedOrder = createSelector(
+	[getOrder, getFilter],
+    (order, filter) => {
+        console.log(order)
+		switch (filter) {
+			case filterStatus.price.f1t9:
+				return order.sort((a, b) => a - b);
+			case filterStatus.price.f9t1:
+				return order.sort((a, b) => b - a);
+			case filterStatus.name.fAtZ:
+				return order.sort((a, b) => a.name.localeCompare(b.name));
+			case filterStatus.name.fZtA:
+				return order.sort((a, b) => b.name.localeCompare(a.name));
+			default:
+				return order;
+		}
+	},
+);
 
 export const getAllData = state => state.data.allData;
 export const getPopData = state => state.data.popData;
